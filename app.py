3b0883a9aa53d3f093af2e4fae9585ead73239bf
tu0315 <takurou.usuki@gmail.com>
2023-12-11 19:48:59 +0900

    Flask超入門03 sha256でエラー出てるけど一旦

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+
+# from flask_bootstrap import Bootstrap
+
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
 from datetime import datetime
 import pytz
 
 # SQLAlchemyの設定
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SECRET_KEY"] = os.urandom(24)
 
 # DB生成
 db = SQLAlchemy(app)
 
+# login_manegerをインスタンス
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+
+# ブログ記事のテーブル情報
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
@@ -21,14 +34,66 @@ class Post(db.Model):
     )
 
 
+# ログインユーザーのテーブル情報
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(12), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 @app.route("/", methods=["GET", "POST"])
+@login_required
 def index():
     if request.method == "GET":
         posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # Userのインスタンスを作成
+        user = User(
+            username=username,
+            password=generate_password_hash(password, method="sha256"),
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/login")
+    else:
+        return render_template("signup.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect("/")
+        else:
+            return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+
 @app.route("/create", methods=["GET", "POST"])
+@login_required
 def create():
     if request.method == "POST":
         title = request.form.get("title")
@@ -40,10 +105,11 @@ def create():
         db.session.commit()
         return redirect("/")
     else:
-        return render_template("create.html")
+        return render_template("login.html")
 
 
 @app.route("/<int:id>/update", methods=["GET", "POST"])
+@login_required
 def update(id):
     post = Post.query.get(id)
 
@@ -57,6 +123,7 @@ def update(id):
 
 
 @app.route("/<int:id>/delete", methods=["GET"])
+@login_required
 def delete(id):
     post = Post.query.get(id)
     db.session.delete(post)
